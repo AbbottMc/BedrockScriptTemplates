eae20cac762521e01957dafe7c14f86247ab4931
QianShanyao <qian_shan_yao@163.com>
2023-01-25 09:55:11 +0800

    初始化项目-1.19.5x_原生

---
+++ bev/scripts/Main.js
@@ -0,0 +1,6 @@
+// 在此处编写您的脚本，此处代码仅为示例，可移除。
+import {world} from '@minecraft/server';
+
+world.events.blockPlace.subscribe(({player, block})=>{
+	world.say(`玩家：“${player.name}”放置了id为：“${block.typeId}”的方块`);
+});
